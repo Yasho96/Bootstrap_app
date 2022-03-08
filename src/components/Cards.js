47e471ba9c029f16ca4cf97cdc044a6367bbb555
Yasho96 <trisp7777@gmail.com>
2022-03-08 13:27:51 +0530

    cards commit 4

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,25 +6,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faStar,
   faStarHalfAlt,
-  faVideo,
+  faCartShopping,
   faDownload,
 } from "@fortawesome/free-solid-svg-icons";
 export const Cards = () => {
     return (
+        
       <ReactBootStrap.Container center className="my-card-container">   
         <ReactBootStrap.Card.Title className="title-1">NEW PRODUCTS</ReactBootStrap.Card.Title>     
         <ReactBootStrap.Row xs={1} md={4} className="cards-row-1">
+        <ReactBootStrap.Carousel>
+             <ReactBootStrap.Carousel.Item interval={1000}>
                 <ReactBootStrap.Col>
                     <ReactBootStrap.Card className="card1">
                     <ReactBootStrap.Card.Img className="card1-img" src= "https://149349728.v2.pressablecdn.com/wp-content/uploads/2019/03/1*u_ULYT4pOBHhBx5bO4596Q.png" />
-                        <ReactBootStrap.Card.Body>
+                        <ReactBootStrap.Card.Body className="card-body">
                             <ReactBootStrap.Card.Text className= "card-price">LKR 1000</ReactBootStrap.Card.Text>
                             <ReactBootStrap.Card.Title className="title">SHERLOCK HOLMES <br/> The man with the twisted lip</ReactBootStrap.Card.Title>
-                            <ReactBootStrap.Card.Text className="text-muted">
-                                Be the first one to write a review
-                            </ReactBootStrap.Card.Text>                                                     
-                        </ReactBootStrap.Card.Body>
-                        <ReactBootStrap.Card.Body className="rating-group">
+                            <ReactBootStrap.Card.Text className="card-discription">
+                                    Be the first one to write a review
+                        </ReactBootStrap.Card.Text>                            
+                            <div className="rating-group">
                                 <div className="star-group">
                                     <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
                                     <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
@@ -37,10 +39,15 @@ export const Cards = () => {
                                     <FontAwesomeIcon className="star" icon={faStar} color="#d9d9d9" />
                                 </div>
                                 <div className="rating-amount">4.8</div>
-                        </ReactBootStrap.Card.Body>  
-                        <ReactBootStrap.Card.Footer className="footer">  
-                            <div className="icon-section">                                
-                                <ReactBootStrap.Button id="Add-to-cart" variant="dark"><img className="icon1" src="https://flaticons.net/icon.php?slug_category=shopping&slug_icon=add-to-cart" alt=""/>Add to cart</ReactBootStrap.Button>
+                            </div>
+                            <div className="icon-section">
+                                <div className="icon-group">
+                                        <FontAwesomeIcon
+                                            className="icon"
+                                            icon={faCartShopping}
+                                            color="#d9d9d9"
+                                        />
+                                </div>
                                 <div className="icon-group">
                                         <FontAwesomeIcon
                                             className="icon"
@@ -49,23 +56,24 @@ export const Cards = () => {
                                         />
                                         <div className="iconText">125</div>
                                 </div>
-                            </div>
-                        </ReactBootStrap.Card.Footer>
+                            </div>                                                            
+                        </ReactBootStrap.Card.Body>
                     </ReactBootStrap.Card>                    
                 </ReactBootStrap.Col>
+                </ReactBootStrap.Carousel.Item>
+                <ReactBootStrap.Carousel.Item interval={1000}>
                 <ReactBootStrap.Col>
                     <ReactBootStrap.Card className="card2">
                         <ReactBootStrap.OverlayTrigger overlay={ <ReactBootStrap.Tooltip id={'tootlip'}> Limited </ReactBootStrap.Tooltip> }>
                             <ReactBootStrap.Card.Img className="card2-img" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/a7159230480213.56057cb033583.png" />
                         </ReactBootStrap.OverlayTrigger>                       
-                        <ReactBootStrap.Card.Body>
-                        <ReactBootStrap.Card.Text className= "card-price">LKR 1000</ReactBootStrap.Card.Text>
-                        <ReactBootStrap.Card.Title className="title" >THE LORD OF THE RINGS <br/> The return of the king</ReactBootStrap.Card.Title>
-                        <ReactBootStrap.Card.Text>
-                                Be the first one to write a review
-                            </ReactBootStrap.Card.Text>
-                        </ReactBootStrap.Card.Body> 
-                        <ReactBootStrap.Card.Body className="rating-group">
+                        <ReactBootStrap.Card.Body className="card-body">
+                            <ReactBootStrap.Card.Text className= "card-price">LKR 1000</ReactBootStrap.Card.Text>
+                            <ReactBootStrap.Card.Title className="title" >THE LORD OF THE RINGS <br/> The return of the king</ReactBootStrap.Card.Title>
+                            <ReactBootStrap.Card.Text className="card-discription">
+                                        Be the first one to write a review
+                            </ReactBootStrap.Card.Text>                            
+                            <div className="rating-group">
                                 <div className="star-group">
                                     <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
                                     <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
@@ -78,10 +86,15 @@ export const Cards = () => {
                                     <FontAwesomeIcon className="star" icon={faStar} color="#d9d9d9" />
                                 </div>
                                 <div className="rating-amount">4.8</div>
-                        </ReactBootStrap.Card.Body>                        
-                        <ReactBootStrap.Card.Footer className="footer">  
-                            <div className="icon-section">                                
-                                <ReactBootStrap.Button id="Add-to-cart" variant="dark"><img className="icon1" src="https://flaticons.net/icon.php?slug_category=shopping&slug_icon=add-to-cart" alt=""/>Add to cart</ReactBootStrap.Button>
+                            </div> 
+                            <div className="icon-section">
+                                <div className="icon-group">
+                                        <FontAwesomeIcon
+                                            className="icon"
+                                            icon={faCartShopping}
+                                            color="#d9d9d9"
+                                        />
+                                </div>
                                 <div className="icon-group">
                                         <FontAwesomeIcon
                                             className="icon"
@@ -90,23 +103,24 @@ export const Cards = () => {
                                         />
                                         <div className="iconText">125</div>
                                 </div>
-                            </div>
-                        </ReactBootStrap.Card.Footer>
+                            </div>                               
+                        </ReactBootStrap.Card.Body> 
                     </ReactBootStrap.Card>
                 </ReactBootStrap.Col>
+                </ReactBootStrap.Carousel.Item>
+                <ReactBootStrap.Carousel.Item interval={1000}>
                 <ReactBootStrap.Col>
                     <ReactBootStrap.Card className="card3">
                         <ReactBootStrap.OverlayTrigger overlay={ <ReactBootStrap.Tooltip id={'tootlip'}> Limited </ReactBootStrap.Tooltip> }>
                         <ReactBootStrap.Card.Img className="card3-img" src="https://assets.mugglenet.com/wp-content/uploads/2021/02/De-Lucchi-all-Harry-Potter-covers.jpg" />
                         </ReactBootStrap.OverlayTrigger>                        
-                        <ReactBootStrap.Card.Body>
+                        <ReactBootStrap.Card.Body className="card-body">
                         <ReactBootStrap.Card.Text className= "card-price">LKR 1000</ReactBootStrap.Card.Text>
                         <ReactBootStrap.Card.Title className="title" >HARRY POTTER <br/> Deathly Hollows</ReactBootStrap.Card.Title>
-                        <ReactBootStrap.Card.Text>
-                                Be the first one to write a review
-                            </ReactBootStrap.Card.Text>
-                        </ReactBootStrap.Card.Body>
-                        <ReactBootStrap.Card.Body className="rating-group">
+                        <ReactBootStrap.Card.Text className="card-discription">
+                                    Be the first one to write a review
+                        </ReactBootStrap.Card.Text>                            
+                            <div className="rating-group">
                                 <div className="star-group">
                                     <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
                                     <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
@@ -119,10 +133,15 @@ export const Cards = () => {
                                     <FontAwesomeIcon className="star" icon={faStar} color="#d9d9d9" />
                                 </div>
                                 <div className="rating-amount">4.8</div>
-                        </ReactBootStrap.Card.Body>                        
-                        <ReactBootStrap.Card.Footer className="footer">  
-                            <div className="icon-section">                                
-                                <ReactBootStrap.Button id="Add-to-cart" variant="dark"><img className="icon1" src="https://flaticons.net/icon.php?slug_category=shopping&slug_icon=add-to-cart" alt=""/>Add to cart</ReactBootStrap.Button>
+                            </div>  
+                            <div className="icon-section">
+                                <div className="icon-group">
+                                        <FontAwesomeIcon
+                                            className="icon"
+                                            icon={faCartShopping}
+                                            color="#d9d9d9"
+                                        />
+                                </div>
                                 <div className="icon-group">
                                         <FontAwesomeIcon
                                             className="icon"
@@ -131,21 +150,22 @@ export const Cards = () => {
                                         />
                                         <div className="iconText">125</div>
                                 </div>
-                            </div>
-                        </ReactBootStrap.Card.Footer>
+                            </div>                                
+                        </ReactBootStrap.Card.Body>
                     </ReactBootStrap.Card>
                 </ReactBootStrap.Col>
+                </ReactBootStrap.Carousel.Item>
+                <ReactBootStrap.Carousel.Item interval={1000}>
                 <ReactBootStrap.Col>
                     <ReactBootStrap.Card className="card4">
                         <ReactBootStrap.Card.Img className="card4-img" src="https://agathachristie.imgix.net/image-store/Landing-Pages-2021/Square_DOTNWebsite.jpg?auto=compress,format&fit=crop&q=80&w=300" />
-                        <ReactBootStrap.Card.Body>
+                        <ReactBootStrap.Card.Body className="card-body">
                         <ReactBootStrap.Card.Text className= "card-price">LKR 1000</ReactBootStrap.Card.Text>
                         <ReactBootStrap.Card.Title className="title" >AGATHA CHRISTIE <br/> Death of the Nile </ReactBootStrap.Card.Title>
-                        <ReactBootStrap.Card.Text>
+                        <ReactBootStrap.Card.Text className="card-discription">
                                     Be the first one to write a review
-                                </ReactBootStrap.Card.Text>
-                            </ReactBootStrap.Card.Body>
-                            <ReactBootStrap.Card.Body className="rating-group">
+                        </ReactBootStrap.Card.Text>                            
+                            <div className="rating-group">
                                 <div className="star-group">
                                     <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
                                     <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
@@ -158,10 +178,15 @@ export const Cards = () => {
                                     <FontAwesomeIcon className="star" icon={faStar} color="#d9d9d9" />
                                 </div>
                                 <div className="rating-amount">4.8</div>
-                        </ReactBootStrap.Card.Body>                        
-                        <ReactBootStrap.Card.Footer className="footer">  
-                            <div className="icon-section">                                
-                                <ReactBootStrap.Button id="Add-to-cart" variant="dark"><img className="icon1" src="https://flaticons.net/icon.php?slug_category=shopping&slug_icon=add-to-cart" alt=""/>Add to cart</ReactBootStrap.Button>
+                            </div> 
+                            <div className="icon-section"> 
+                                <div className="icon-group">
+                                        <FontAwesomeIcon
+                                            className="icon"
+                                            icon={faCartShopping}
+                                            color="#d9d9d9"
+                                        />
+                                </div>
                                 <div className="icon-group">
                                         <FontAwesomeIcon
                                             className="icon"
@@ -170,10 +195,57 @@ export const Cards = () => {
                                         />
                                         <div className="iconText">125</div>
                                 </div>
-                            </div>
-                        </ReactBootStrap.Card.Footer>
+                            </div>                                
+                        </ReactBootStrap.Card.Body>
                     </ReactBootStrap.Card>
                 </ReactBootStrap.Col>
+                </ReactBootStrap.Carousel.Item>
+                 <ReactBootStrap.Carousel.Item interval={1000}>
+                <ReactBootStrap.Col>
+                    <ReactBootStrap.Card className="card4">
+                        <ReactBootStrap.Card.Img className="card4-img" src="https://agathachristie.imgix.net/news/Inline_ABCExtract.png" />
+                        <ReactBootStrap.Card.Body className="card-body">
+                        <ReactBootStrap.Card.Text className= "card-price">LKR 1000</ReactBootStrap.Card.Text>
+                        <ReactBootStrap.Card.Title className="title" >AGATHA CHRISTIE <br/> The ABC Murders </ReactBootStrap.Card.Title>
+                        <ReactBootStrap.Card.Text className="card-discription">
+                                    Be the first one to write a review
+                        </ReactBootStrap.Card.Text>                            
+                            <div className="rating-group">
+                                <div className="star-group">
+                                    <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
+                                    <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
+                                    <FontAwesomeIcon className="star" icon={faStar} color="#FFCC00" />
+                                    <FontAwesomeIcon
+                                        className="star"
+                                        icon={faStarHalfAlt}
+                                        color="#FFCC00"
+                                    />
+                                    <FontAwesomeIcon className="star" icon={faStar} color="#d9d9d9" />
+                                </div>
+                                <div className="rating-amount">4.8</div>
+                            </div> 
+                            <div className="icon-section"> 
+                                <div className="icon-group">
+                                        <FontAwesomeIcon
+                                            className="icon"
+                                            icon={faCartShopping}
+                                            color="#d9d9d9"
+                                        />
+                                </div>
+                                <div className="icon-group">
+                                        <FontAwesomeIcon
+                                            className="icon"
+                                            icon={faDownload}
+                                            color="#d9d9d9"
+                                        />
+                                        <div className="iconText">125</div>
+                                </div>
+                            </div>                                
+                        </ReactBootStrap.Card.Body>
+                    </ReactBootStrap.Card>
+                </ReactBootStrap.Col>
+                </ReactBootStrap.Carousel.Item>
+            </ReactBootStrap.Carousel>
         </ReactBootStrap.Row>
       </ReactBootStrap.Container>
       
